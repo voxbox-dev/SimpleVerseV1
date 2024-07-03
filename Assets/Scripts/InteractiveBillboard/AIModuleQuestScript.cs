3f62e6607611ba87183bb2cab4e082f07e45d96e
Rhyguy <rhyan@rhyanvargas.com>
2024-07-03 16:59:30 -0700

    add AIModule scriptable objects, billboard manager and quest script

---
+++ Assets/Scripts/InteractiveBillboard/AIModuleQuestScript.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Simpleverse
+{
+    public class AIModuleQuestScript : MonoBehaviour
+    {
+        [SerializeField] private AIModuleQuestSO aimoduleQuestSO;
+
+        private List<Transform> updatedBillboardPositions = new List<Transform>();
+
+        public void UpdateBillboards(int numberOfBillboardsToUpdate)
+        {
+            List<Transform> billboardPositions = BillboardManager.Instance.billboardPositions;
+            // Randomly select and replace X billboards with X billboards from the quest
+            for (int i = 0; i < numberOfBillboardsToUpdate; i++)
+            {
+                int positionIndex = Random.Range(0, billboardPositions.Count);
+                int questBillboardIndex = Random.Range(0, aimoduleQuestSO.questBillboardPrefabs.Count);
+
+                Transform positionToReplace = billboardPositions[positionIndex];
+                GameObject questBillboardPrefab = aimoduleQuestSO.questBillboardPrefabs[questBillboardIndex];
+
+                // Replace the billboard
+                GameObject newBillboard = Instantiate(questBillboardPrefab, positionToReplace.position, positionToReplace.rotation, positionToReplace);
+                positionToReplace.gameObject.SetActive(false);
+
+                // Keep track of updated billboard positions
+                updatedBillboardPositions.Add(positionToReplace);
+            }
+        }
+
+        public void ResetBillboards()
+        {
+            // Revert the changed billboards with defaultBillboardPrefab
+            foreach (Transform updatedPosition in updatedBillboardPositions)
+            {
+                updatedPosition.gameObject.SetActive(true);
+                // Instantiate the default prefab at the position
+                Instantiate(BillboardManager.Instance.defaultBillboardPrefab, updatedPosition.position, updatedPosition.rotation, updatedPosition);
+
+                // Destroy the quest billboard
+                foreach (Transform child in updatedPosition)
+                {
+                    Destroy(child.gameObject);
+                }
+            }
+
+            // Clear the list after resetting
+            updatedBillboardPositions.Clear();
+        }
+    }
+}
--- Assets/Scripts/InteractiveBillboard/BillboardManager.cs
+++ Assets/Scripts/InteractiveBillboard/BillboardManager.cs
@@ -1,16 +1,37 @@
+using System.Collections;
 using System.Collections.Generic;
-using SpatialSys.UnitySDK;
 using UnityEngine;
-using UnityEngine.UI; // Required for accessing the Image component
 
-namespace Simpleverse {
-    /// <summary>
-    /// Manages the billboards in the scene
-    /// </summary>
+namespace Simpleverse
+{
     public class BillboardManager : MonoBehaviour
     {
+        public static BillboardManager Instance { get; private set; }
+         public GameObject defaultBillboardPrefab;
+        public List<Transform> billboardPositions; // Assign in-scene billboards here in the inspector
 
-       
+        private void Awake()
+        {
+            Debug.Log("Awake called");
+            if (Instance != null && Instance != this)
+            {
+                Debug.Log("Destroying duplicate instance of VehicleManager");
+                Destroy(this);
+            }
+            else
+            {
+                Debug.Log("Setting instance of VehicleManager");
+                Instance = this;
+                InitializeBillboards();
+            }
+        }
+
+        private void InitializeBillboards()
+    {
+        foreach (Transform position in billboardPositions)
+        {
+            Instantiate(defaultBillboardPrefab, position.position, position.rotation, position);
+        }
     }
-
+    }
 }
