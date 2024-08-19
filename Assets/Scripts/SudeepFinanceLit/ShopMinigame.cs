6ba26cc51d4baa3c9c91d46a3466a462cfa44982
SudeepBiswa <125826645+SudeepBiswa@users.noreply.github.com>
2024-08-19 08:02:04 -0500

    add minigame for quest 22
    - add timer later after finishing other quests

--- Assets/Scripts/SudeepFinanceLit/ShopMinigame.cs
+++ Assets/Scripts/SudeepFinanceLit/ShopMinigame.cs
@@ -9,26 +9,45 @@ namespace Simpleverse
         public GameObject[] items;
         public float[] prices;
 
+        public GameObject cashier;
         public float budget;
 
         private List<GameObject> pickupItems;
         private List<float> itemPrice;
+        private List<GameObject> pickedUp;
 
         private float score;
+
+        private string state;
         // Start is called before the first frame update
         void Start()
         {
             score = 0;
             pickupItems = new List<GameObject>(items);
             itemPrice = new List<float>(prices);
+            pickedUp = new List<GameObject>();
+            state = "START";
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (transform.gameObject.activeSelf)
+            if (state.Equals("START"))
             {
+                start();
+            }
+            else if (state.Equals("PLAY"))
+            {
+                
                 itemPickup();
+            }
+            else if (state.Equals("STOP"))
+            {
+                finishGame();
+            }
+            else if (state.Equals("REPLAY"))
+            {
+                replay();
             }
         }
 
@@ -39,10 +58,56 @@ namespace Simpleverse
                 if (!pickupItems[i].activeSelf)
                 {
                     score += itemPrice[i];
+                    pickedUp.Add(pickupItems[i]);
                     pickupItems.Remove(pickupItems[i]);
                     itemPrice.Remove(i);
                     i--;
                 }
+            }
+            if (!transform.Find("FinishMinigame").gameObject.activeSelf)
+            {
+                state = "STOP";
+            }
+        }
+
+        void finishGame()
+        {
+            if(pickedUp.Count < 4 || score > budget)
+            {
+                transform.Find("TryAgain").gameObject.SetActive(true);
+                state = "REPLAY";
+            }
+            else
+            {
+                transform.Find("CompleteQuiz").gameObject.SetActive(true);
+                transform.Find("PicupItems").gameObject.SetActive(false);
+            }
+            
+        }
+
+        void replay()
+        {
+            if (!transform.Find("TryAgain").gameObject.activeSelf)
+            {
+                //transform.Find("TryAgain").gameObject.SetActive(false);
+                score = 0;
+                pickupItems = new List<GameObject>(items);
+                foreach(GameObject obj in pickupItems)
+                {
+                    obj.gameObject.SetActive(true);
+                }
+                itemPrice = new List<float>(prices);
+                pickedUp = new List<GameObject>();
+                state = "PLAY";
+
+            }
+        }
+
+        void start()
+        {
+            if (!transform.Find("StartMinigame").gameObject.activeSelf)
+            {
+                state = "PLAY";
             }
         }
     }
